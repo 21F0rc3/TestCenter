7e364ae8a19af5a953d7d3c681a2344f61872883
21F0rc3 <force2606@gmail.com>
2020-06-13 12:07:35 +0100

    Otimização do codigo

--- code.js
+++ code.js
@@ -1,16 +1,9 @@
-var IndexDeQuestoes = 0;
-var arrayQuestoes;
+var arrayQuestoes, time, resposta, text, segundos, nrDeSelecionada, score, IndexDeQuestoes, minutos;  //Criação de algumas variaveis
 
-var minutos = 1;
-var segundos = 0;
+minutos = 1;    //Definição dos minutos do teste
+segundos = 0;   //Definição dos segundos do teste
 
-var nrDeSelecionada = 0;
-
-var score = 0;
-
-var resposta;
-
-var text;
+IndexDeQuestoes = nrDeSelecionada = score = 0;
 
 function start() {
   var xmlhttp = new XMLHttpRequest();
@@ -35,54 +28,36 @@ function start() {
 }
 
 function fillQuestoes(arrayQuestoes) {
-  var teste = $("#Test");
-  let tam = arrayQuestoes.length;
-  for(i=0; i<tam; i++) {
-    $("#Test").append("<div id='Questao"+i+"' class='Quest'></div>");
-
-    $("#Questao"+i).append("<h1 class='Questao'>"+arrayQuestoes[i].pergunta+"</h1>");
-    $("#Questao"+i).css("display", "none");
-
-    if(arrayQuestoes[i].tipo == "escolha unica") {
-      for(j=0; j<4; j++) {
-        $("#Questao"+i).append("<h2 id='Opcao_"+j+"' class='Opcao'>"+arrayQuestoes[i].opcoes[j]+"</h2>");
-        $("#Questao"+i).children("#Opcao_"+j).bind("click", opcaoSelecionada);
-      }
-    }
-
-    if(arrayQuestoes[i].tipo == "escolha multipla") {
-      for(j=0; j<4; j++) {
-        $("#Questao"+i).append("<h2 id='Opcao_"+j+"' class='Opcao'>"+arrayQuestoes[i].opcoes[j]+"</h2>");
-        $("#Questao"+i).children("#Opcao_"+j).bind("click", opcaoSelecionada);
-      }
-    }
-
-    if(arrayQuestoes[i].tipo == "V ou F") {
-      for(j=0; j<2; j++) {
-        $("#Questao"+i).append("<h2 id='Opcao_"+j+"' class='Opcao'>"+arrayQuestoes[i].opcoes[j]+"</h2>");
-        $("#Questao"+i).children("#Opcao_"+j).bind("click", opcaoSelecionada);
-      }
-    }
+  for(i=0; i<arrayQuestoes.length; i++) {
+    $("#Test").append("<div id='Questao"+i+"'></div>");
+    $("#Questao"+i).append("<h1 class='Questao'>"+arrayQuestoes[i].pergunta+"</h1>").css("display", "none");
 
     if(arrayQuestoes[i].tipo == "completar") {
       $("#Questao"+i).append("<input class='completar' type='text' value=''>");
       $("#Questao"+i).children("input").change(checkTexto);
+    }else {
+      for(j=0; j<arrayQuestoes[i].opcoes.length; j++) {
+        $("#Questao"+i).append("<h2 id='Opcao_"+j+"' class='Opcao'>"+arrayQuestoes[i].opcoes[j]+"</h2>");
+        $("#Questao"+i).children("#Opcao_"+j).bind("click", opcaoSelecionada);
+      }
+    }
   }
   $("#Questao0").css("display", "block");
 }
-}
 
 function checkTexto() {
-  if($(this).val() == arrayQuestoes[IndexDeQuestoes].resposta) {
-    if($(this).parent().attr("value") == "respondidoCorretamente") {
-    }else {
-      score+=5;
-      $(this).parent().attr("value", "respondidoCorretamente");
+  if($(this).parent().attr("value") == "respondidoCorretamente") {
+    if($(this).val() != arrayQuestoes[IndexDeQuestoes].resposta) {
+      score-=5;
+      $(this).parent().attr("value", "");
     }
-  }else if($(this).parent().attr("value") == "respondidoCorretamente") {
-    score-=5;
-    $(this).parent().attr("value", "");
+  }else if($(this).val() == arrayQuestoes[IndexDeQuestoes].resposta) {
+    score+=5;
+    $(this).parent().attr("value", "respondidoCorretamente");
   }
+
+  verifSeRespondeu();
+
   return score;
 }
 
@@ -105,9 +80,11 @@ function opcaoSelecionada() {
   }
   var select = $(this);
   checkResposta(select);
-  //alert(score);        Comando para testar se a funcao esta a contar em tempo real
+  verifSeRespondeu();
+  //alert(score);        //Comando para testar se a funcao esta a contar em tempo real
   return nrDeSelecionada;
 }
+
 
 function perguntaAnterior() {
   $("#Questao"+IndexDeQuestoes).css("display", "none");
@@ -126,17 +103,14 @@ function perguntaAnterior() {
 
 function perguntaSeguinte() {
   $("#Questao"+IndexDeQuestoes).children(".Opcao").unbind("click");
-  $("#Questao"+IndexDeQuestoes).children(".Opcao").attr("class", "Opcoes");
   $("#Questao"+IndexDeQuestoes).children("input").attr("readonly", true);
   $("#Questao"+IndexDeQuestoes).css("display", "none");
-
-  verifSeRespondeu();
 
   IndexDeQuestoes++;
 
   $("#Questao"+IndexDeQuestoes).css("display", "inline");
 
-  if(IndexDeQuestoes >= arrayQuestoes.length - 1) {
+  if(IndexDeQuestoes == arrayQuestoes.length - 1) {
     $("#next").css("display", "none");
   }
 
@@ -144,17 +118,25 @@ function perguntaSeguinte() {
     $("#prev").css("display", "inline");
   }
 
+  resetNrDeSelecionadas();
+
   return IndexDeQuestoes;
 }
 
+function resetNrDeSelecionadas() {
+  nrDeSelecionada = 0;
+  return nrDeSelecionada;
+}
+
 function acabarTeste() {
-  verifSeRespondeu();
+  clearInterval(time);
   var nrDeRespondidas = 0;
   var nrDeRespostasCertas = 0;
   for(i=0; i<arrayQuestoes.length; i++) {
     if($("#Questao"+i).hasClass("respondido")) {
       nrDeRespondidas++;
     }
+
     if(arrayQuestoes[i].tipo == "escolha multipla") {
       for(j=0; j<4; j++) {
         if($("#Questao"+i).children("#Opcao_"+j).attr("value") == "respondidoCorretamente") {
@@ -168,66 +150,64 @@ function acabarTeste() {
       }
     }
   }
-  var nrdeNaoRespondidas = arrayQuestoes.length - nrDeRespondidas;
-  var nrDeRespostasErr = arrayQuestoes.length - nrdeNaoRespondidas - nrDeRespostasCertas;
-  $(".modal-title").append("Resultados");
-  $(".modal-body").append("<div>Score: "+score+"</div>");
-  $(".modal-body").append("<div>Respostas não respondidas: "+nrdeNaoRespondidas+"</div>");
-  $(".modal-body").append("<div>Respostas certas: "+nrDeRespostasCertas+"</div>");
-  $(".modal-body").append("<div>Respostas erradas: "+nrDeRespostasErr+"</div>");
+  var nrDeNaoRespondidas = arrayQuestoes.length - nrDeRespondidas;
+  var nrDeRespostasErr = nrDeRespondidas - nrDeRespostasCertas;
+  $(".modal-title").append("Resultados:");
+  $(".modal-body").append("<div>Score: "+score+"/40</div>");
+  $(".modal-body").append("<div>Respostas não respondidas: "+nrDeNaoRespondidas+"/8</div>");
+  $(".modal-body").append("<div>Respostas certas: "+nrDeRespostasCertas+"/"+nrDeRespondidas+"</div>");
+  $(".modal-body").append("<div>Respostas erradas: "+nrDeRespostasErr+"/"+nrDeRespondidas+"</div>");
   $("#finish").css("display", "none");
 }
 
 function verifSeRespondeu() {
-  if(arrayQuestoes[IndexDeQuestoes].tipo == "escolha unica" || arrayQuestoes[IndexDeQuestoes].tipo == "escolha multipla") {
-    for(i=0; i<4; i++) {
-      if($("#Questao"+IndexDeQuestoes).children("#Opcao_"+i).css("backgroundColor") == "rgb(0, 0, 0)") {
-        $("#Questao"+IndexDeQuestoes).addClass("respondido");
-      }
-    }
-  }else if(arrayQuestoes[IndexDeQuestoes].tipo == "V ou F") {
-    for(i=0; i<2; i++) {
-      if($("#Questao"+IndexDeQuestoes).children("#Opcao_"+i).css("backgroundColor") == "rgb(0, 0, 0)") {
-        $("#Questao"+IndexDeQuestoes).addClass("respondido");
-      }
-    }
-  }else if(arrayQuestoes[IndexDeQuestoes].tipo == "completar") {
-    if($("#Questao"+IndexDeQuestoes).children("input").val() == "") {
-    }else{
+  if(arrayQuestoes[IndexDeQuestoes].tipo == "completar") {
+    if($("#Questao"+IndexDeQuestoes).children("input").val() != "") {
       $("#Questao"+IndexDeQuestoes).addClass("respondido");
+    }else{
+      $("#Questao"+IndexDeQuestoes).removeClass("respondido");
+    }
+  }else{
+    for(i=0; i<arrayQuestoes[IndexDeQuestoes].opcoes.length; i++) {
+      if($("#Questao"+IndexDeQuestoes).children("#Opcao_"+i).css("backgroundColor") == "rgb(0, 0, 0)") {
+        $("#Questao"+IndexDeQuestoes).addClass("respondido");
+        break;
+      }else{
+        $("#Questao"+IndexDeQuestoes).removeClass("respondido");
+      }
     }
   }
 }
 
 function checkResposta(select) {
-  if(arrayQuestoes[IndexDeQuestoes].tipo == "escolha unica" || arrayQuestoes[IndexDeQuestoes].tipo == "V ou F") {
+  if(arrayQuestoes[IndexDeQuestoes].tipo == "escolha multipla") {
+    if($(select).text() == arrayQuestoes[IndexDeQuestoes].resposta[0] || $(select).text() == arrayQuestoes[IndexDeQuestoes].resposta[1]) {
+      if($(select).attr("value") == "respondidoCorretamente") {
+        score-=2.5;
+        $(select).attr("value", "");
+      }else{
+        score+=2.5;
+        $(select).attr("value", "respondidoCorretamente");
+      }
+    }
+  }else {
     if($(select).parent().attr("value") == "respondidoCorretamente") {
-        score-=5;
-        $(select).parent().attr("value", "");
+      score-=5;
+      $(select).parent().attr("value", "");
     }else if($(select).text() == arrayQuestoes[IndexDeQuestoes].resposta) {
       score+=5;
       $(select).parent().attr("value", "respondidoCorretamente");
     }
-  }else{
-      if($(select).text() == arrayQuestoes[IndexDeQuestoes].resposta[0] || $(select).text() == arrayQuestoes[IndexDeQuestoes].resposta[1]) {
-        if($(select).attr("value") == "respondidoCorretamente") {
-          score-=2.5;
-          $(select).attr("value", "");
-        }else{
-          score+=2.5;
-          $(select).attr("value", "respondidoCorretamente");
-        }
-      }
   }
   return score;
 }
 
 function countdown() {
-  var time = setInterval(function () {
+  time = setInterval(function () {
      if(minutos == 0 && segundos == 0) {
-       $(".modal-title").append("Acabou o tempo!");
+       $(".modal-title").append("Acabou o tempo! ");
        acabarTeste();
-       clearInterval(time);
+       $("#acabarTeste").modal("toggle");
      }else if(segundos > 0) {
        segundos--;
      }else{
